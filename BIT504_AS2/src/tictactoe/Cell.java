1f9c6302671d1a06e982f7eaa6a31a6864528501
ironickitkat <kakapokalene@gmail.com>
2024-08-20 20:28:29 +1200

    Changes 20-08-24
    Cell:
    •	Initialised variables row & col
    •	Written method that empties cells
    •	Calls method that sets cells to empty
    Board:
    •	Initialised cells on board using cells and rows
    •	Written a method to check if the game is a draw
    •	Finished the hasWon method by 3 in a col row and the other diagonal
    direction
    GameMain
    •	Created an instance of Board class
    •	Call method to initialise game board
    •	Create a GameMain panel and add it to the frame
    •	Set default close operation to exit-on-close
    •	Set status bar message for X’s turn and O’s turn
    •	Created the enumeration for GameState (this was done in the initial
    github upload)

--- BIT504_AS2/src/tictactoe/Cell.java
+++ BIT504_AS2/src/tictactoe/Cell.java
@@ -15,11 +15,12 @@ public class Cell {
 	public Cell(int row, int col) {
 		
 		// TODO: Initialise the variables row, col 
-
+		this.row = row;
+		this.col = col;
 		
 		
 		//TODO: call the method that sets the cell content to EMPTY
-		 
+		clear(); 
 	}
 	
 
@@ -47,8 +48,8 @@ public class Cell {
 	/** Set this cell's content to EMPTY */
 	public void clear() {
 		
-		// TODO: Set the value of content to Empty (Remember this is an enum)
-
+		//Set the value of content to Empty (Remember this is an enum)
+		content = Player.Empty;
 		
 	}
 		
